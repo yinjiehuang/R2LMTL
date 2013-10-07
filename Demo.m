c6451ad925d3f8bb3380cfe46dc204424d9055e8
Yinjie Huang <darrenhuang22@gmail.com>
2013-10-07 03:09:01 -0400

    R2LMTLs

---
+++ Demo.m
@@ -0,0 +1,24 @@
+%This Demo shows how to run R2LMTL
+clear;clc;
+%%Read the data
+path = [pwd,'/Data/ionophsere'];
+addpath([pwd,'/Functions']);
+
+%%We will set all the hyperparameters here
+%Number of metric
+parameters.NumMa_K = 2;
+%Regularization value lambda
+parameters.lambda = 100;
+%Step length of PSD
+parameters.t0 = 1e-6;
+%Number of steps of PSD for each metric
+parameters.iter = 200;
+%Number of epoches of two steps
+parameters.epoch = 5;
+%Number of k-nearest neighbors when testing
+parameters.kneigh = 5;
+
+
+
+%%Run the algorithm
+accu = R2LMTL(path,parameters);
